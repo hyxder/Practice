56e86bf11a1d3cf57d00456c44c56fdf6c23b451
Hyder <164782078+Hyder010@users.noreply.github.com>
2026-04-17 08:30:05 -0400

    First commit

---
+++ Practice/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Practice;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
